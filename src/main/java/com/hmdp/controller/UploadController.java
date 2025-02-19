1f73fb713f777657c8538a8beb78c794b105f923
Wayne-Z <zhangwenyun.zwy@gmail.com>
2025-02-19 15:15:06 +0900

    controller optimized

--- src/main/java/com/hmdp/controller/UploadController.java
+++ src/main/java/com/hmdp/controller/UploadController.java
@@ -20,17 +20,15 @@ public class UploadController {
     @PostMapping("blog")
     public Result uploadImage(@RequestParam("file") MultipartFile image) {
         try {
-            // 获取原始文件名称
             String originalFilename = image.getOriginalFilename();
-            // 生成新文件名
+            // new file name
             String fileName = createNewFileName(originalFilename);
             // save the file locally
             image.transferTo(new File(SystemConstants.IMAGE_UPLOAD_DIR, fileName));
-            // 返回结果
-            log.debug("文件上传成功，{}", fileName);
+            log.debug("Upload Successful，{}", fileName);
             return Result.ok(fileName);
         } catch (IOException e) {
-            throw new RuntimeException("文件上传失败", e);
+            throw new RuntimeException("Upload Failed", e);
         }
     }
 
@@ -38,26 +36,26 @@ public class UploadController {
     public Result deleteBlogImg(@RequestParam("name") String filename) {
         File file = new File(SystemConstants.IMAGE_UPLOAD_DIR, filename);
         if (file.isDirectory()) {
-            return Result.fail("错误的文件名称");
+            return Result.fail("Wrong File Name");
         }
         FileUtil.del(file);
         return Result.ok();
     }
 
     private String createNewFileName(String originalFilename) {
-        // 获取后缀
+        // get the suffix
         String suffix = StrUtil.subAfter(originalFilename, ".", true);
-        // 生成目录
+        // generate directory name
         String name = UUID.randomUUID().toString();
         int hash = name.hashCode();
         int d1 = hash & 0xF;
         int d2 = (hash >> 4) & 0xF;
-        // 判断目录是否存在
+        // check if dir exists
         File dir = new File(SystemConstants.IMAGE_UPLOAD_DIR, StrUtil.format("/blogs/{}/{}", d1, d2));
         if (!dir.exists()) {
             dir.mkdirs();
         }
-        // 生成文件名
+        // generate file name
         return StrUtil.format("/blogs/{}/{}/{}.{}", d1, d2, name, suffix);
     }
 }
