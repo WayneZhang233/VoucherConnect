af48e5d344be7eb1fff0868fa7b3ccd5c4bdd86c
Wayne-Z <zhangwenyun.zwy@gmail.com>
2025-03-02 15:41:14 +0900

    add cdn

--- frontend/html/hmdp/js/common.js
+++ frontend/html/hmdp/js/common.js
@@ -29,9 +29,9 @@ axios.interceptors.response.use(function (response) {
     setTimeout(() => {
       location.href = "/login.html"
     }, 200);
-    return Promise.reject("请先登录");
+    return Promise.reject("Please log in first");
   }
-  return Promise.reject("服务器异常");
+  return Promise.reject("Server Error");
 });
 axios.defaults.paramsSerializer = function(params) {
   let p = "";
